3fdef3fffa9c45d6552697a1d209debdb2ee9a67
darkalit <tkachenkozen@gmail.com>
2021-12-23 12:20:33 +0200

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit d2b8050bd3ab47b1870f9e94705c2e924f7cca14.

---
+++ AsteroidsMSVC/main.cpp
@@ -0,0 +1,17 @@
+#include "Game.h"
+
+int main() {
+
+    // Game engine
+    Game game;
+
+    // Game loop
+    while (game.running()) {
+
+        // Update
+        game.update();
+        // Render
+        game.render();
+    }
+    return 0;
+}
